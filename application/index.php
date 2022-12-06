ba760b3054df39139659cb47ca13547ab83fdf6a
kuokyn <viktoria2nikiforova@gmail.com>
2022-12-06 12:48:56 +0300

    index.php

---
+++ application/index.php
@@ -0,0 +1,17 @@
+<?php
+require('model/database.php');
+require('components/Router.php');
+
+header("Content-type: application/json; charset=UTF-8");
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+define('ROOT', dirname(__FILE__));
+$router = new Router();
+$router->run();
+/*
+ * 1. Общие настройки (включение-выключение ошибок, установка констант и др.)
+ * 2. Подключение файлов системы
+ * 3. Установка соединения с базой данных
+ * 4. Вызов Router
+*/
+
